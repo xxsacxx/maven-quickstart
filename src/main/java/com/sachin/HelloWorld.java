66385f997e10c8fe35047ee2dd35dff70313fb51
xxsacxx <https://gitlab.onionlife.in/>
2020-05-10 19:44:19 +0530

    quickstart-maven

---
+++ src/main/java/com/sachin/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.sachin;
+public class HelloWorld {
+public static void main(String[] args) {
+  HelloWorld myWorld = new HelloWorld();
+  String said = myWorld.sayHelloTo("World");
+  System.out.println(said);
+ }
+ public String sayHelloTo(String to) {
+  return "Hello "+to+"!";
+ }
+}
